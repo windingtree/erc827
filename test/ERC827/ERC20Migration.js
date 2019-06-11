683a3faf060420479a444cb2aee67b3a4470ff37
AugustoL <me@augustol.com>
2019-06-11 13:00:00 -0300

    test(tests): Update test to new dinamyc proxies and add ERC827Reciever example

--- test/ERC827/ERC20Migration.js
+++ test/ERC827/ERC20Migration.js
@@ -1,7 +1,8 @@
 
 import EVMRevert from '../helpers/EVMRevert';
-var ERC20TokenMock = artifacts.require('ERC20TokenMock');
-var ERC827Migratable = artifacts.require('ERC827Migratable');
+const ERC20Mock = artifacts.require('ERC20Mock');
+const ERC827Migratable = artifacts.require('ERC827Migratable');
+const ERC827Proxy = artifacts.require('ERC827Proxy');
 
 require('chai').use(require('chai-as-promised')).should();
 const assert = require('chai').assert;
@@ -10,8 +11,8 @@ contract('ERC827Migratable', function (accounts) {
   let erc20Token, erc827Token;
 
   beforeEach(async function () {
-    erc20Token = await ERC20TokenMock.new(accounts[1], 100);
-    erc827Token = await ERC827Migratable.new(erc20Token.address);
+    erc20Token = await ERC20Mock.new(accounts[1], 100);
+    erc827Token = await ERC827Migratable.new(erc20Token.address, ERC827Proxy.bytecode);
     await erc20Token.transfer(accounts[2], 10, { from: accounts[1] });
     await erc20Token.transfer(accounts[3], 20, { from: accounts[1] });
     await erc20Token.transfer(accounts[4], 30, { from: accounts[1] });
